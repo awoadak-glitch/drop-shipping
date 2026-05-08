c7af52b41be79cd35bba6b26989d676812bd6b86
awoadak-glitch <awoadak@gmail.com>
2026-05-08 17:25:29 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import App from './App.jsx' // تأكد أن المسار صحيح لملف الواجهة
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
