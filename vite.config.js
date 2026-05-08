a3670fda9a5ab816251d35b056b0e6bbe9c03099
awoadak-glitch <awoadak@gmail.com>
2026-05-08 17:15:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/drop-shipping/', // هذا هو اسم مستودعك ليعمل الرابط بشكل صحيح
+  base: '/drop-shipping/', // تأكد أن هذا هو اسم المستودع بالضبط
 })
